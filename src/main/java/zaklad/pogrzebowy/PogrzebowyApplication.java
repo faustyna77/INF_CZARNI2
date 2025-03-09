18db9545e3828cb368a5f98e783e4f69881a45a4
dyjak <dyyyak@gmail.com>
2025-03-09 13:33:38 +0100

    initialization

---
+++ src/main/java/zaklad/pogrzebowy/PogrzebowyApplication.java
@@ -0,0 +1,13 @@
+package zaklad.pogrzebowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PogrzebowyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PogrzebowyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zaklad/pogrzebowy/PogrzebowyApplicationTests.java
@@ -0,0 +1,13 @@
+package zaklad.pogrzebowy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PogrzebowyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
