26a98b0100d65d9ce2e0ce6088599f1d3bfb86ab
fatisssa <fm119041@stud.ur.edu.pl>
2025-04-08 18:42:57 +0200

    database finish

---
+++ src/main/java/zaklad/pogrzebowy/api/models/TaskAssignment.java
@@ -0,0 +1,30 @@
+package zaklad.pogrzebowy.api.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "task_assignments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskAssignment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "task_id", nullable = false)
+    private Task task;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "assigned_at", nullable = false)
+    private LocalDateTime assignedAt = LocalDateTime.now();
+}
