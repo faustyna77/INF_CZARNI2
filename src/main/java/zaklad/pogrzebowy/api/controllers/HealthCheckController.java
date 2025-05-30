badfa5fbb23e955cfde9b62dae0e66fd73a78c90
Faustyna Misiura <110495453+faustyna77@users.noreply.github.com>
2025-05-30 19:44:01 +0200

    Update HealthCheckController.java

--- src/main/java/zaklad/pogrzebowy/api/controllers/HealthCheckController.java
+++ src/main/java/zaklad/pogrzebowy/api/controllers/HealthCheckController.java
@@ -1,15 +1,13 @@
 package zaklad.pogrzebowy.api.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
-public class HealthCheckController {
+public class HealthController {
 
-    @GetMapping("/health")
-    public String checkHealth() {
+    @GetMapping("/api/health")
+    public String healthCheck() {
         return "OK";
     }
 }
